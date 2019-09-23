bceb4ef98b7b5c90e3f7a496c19b79478a2e8668
tanzilsheikh265521 <53473299+tanzilsheikh265521@users.noreply.github.com>
2019-09-23 16:31:12 +0530

    Add files via upload
    c program for printing the sum of even squares of n intergers

---
+++ N-numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+	int n, i, sum=0;
+
+	scanf("%d",&n);
+
+	for(i=1; i<= 2*n; i++)
+
+	if(i%2==0)
+
+	sum= sum + i*i;
+
+	printf("Sum of square of %d even numbers is %d\n", n, sum);
+
+	return 0;
+
+}
